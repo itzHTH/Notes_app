0fd776c617828d4d0c16374e464b9ccba56625c7
Leader <jhuth981@gmail.com>
2024-09-19 01:48:36 +0300

    Add note functionality to AddNoteCubit
    - This commit introduces the ability to add notes to the `AddNoteCubit` class. The `addNote` method is implemented to handle the addition of a `NoteModel` to a Hive box. The method emits `AddNoteLoading` state during the process, `AddNoteSuccess` upon successful addition, and `AddNoteFailure` with an error message if an exception occurs.

--- lib/cubits/cubit/add_note_cubit.dart
+++ lib/cubits/cubit/add_note_cubit.dart
@@ -1,8 +1,21 @@
-
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:notes_app/constans.dart';
+import 'package:notes_app/models/note_model.dart';
 
 part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
+
+  addNote(NoteModel note) async {
+    emit(AddNoteLoading());
+    try {
+      var notesBox = Hive.box(kNoteBox);
+      await notesBox.add(note);
+      emit(AddNoteSuccess());
+    } on Exception catch (e) {
+      emit(AddNoteFailure(errMessage: e.toString()));
+    }
+  }
 }
