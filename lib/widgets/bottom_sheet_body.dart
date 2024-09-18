d46cc79fcd62dc0e4cabf5ae5ea84af98722da13
Leader <jhuth981@gmail.com>
2024-09-19 00:28:55 +0300

    Add AddNote Cubit and make state
    - Created a new `AddNoteCubit` class in `lib/cubits/cubit/add_note_cubit.dart` to manage the state for adding notes. This includes the initial state, loading state, success state, and failure state with an error message.
    
    - Also, added the `AddNoteState` abstract class and its concrete implementations in `lib/cubits/cubit/add_note_state.dart`.

--- lib/widgets/bottom_sheet_body.dart
+++ lib/widgets/bottom_sheet_body.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:notes_app/constans.dart';
 import 'package:notes_app/widgets/custom_button.dart';
 import 'package:notes_app/widgets/custom_text_filed.dart';
 
